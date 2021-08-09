e2a2bda7ea8d76d4ce95caf050eeb235b8f67811
Jinwoo Kim <kidjinwoo@me.com>
2021-08-09 18:46:00 +0900

    minor fix

--- AppStoreSearchDemoCore/Domain/Entity/Internal/RecentKeyword.swift
+++ AppStoreSearchDemoCore/Domain/Entity/Internal/RecentKeyword.swift
@@ -12,4 +12,12 @@ final class RecentKeyword: Object {
     @objc @Persisted(primaryKey: true) dynamic var identity: UUID = .init()
     @objc @Persisted dynamic var keyword: String? = nil
     @objc @Persisted dynamic var timestamp: Date = Date()
+    
+//    override func isEqual(_ object: Any?) -> Bool {
+//        guard let object: RecentKeyword = object as? RecentKeyword else {
+//            return super.isEqual(object)
+//        }
+//        
+//        return self.identity == object.identity
+//    }
 }
